5b20c3bbc3baa3e386e845180e25979076ae37c4
Udara Neminda Chandrawansa <udaraneminda@gmail.com>
2025-08-11 19:19:50 +0530

    telescope fix #1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,10 +9,13 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void
+    public function register()
     {
-        //
+        if ($this->app->environment('local')) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+        }
     }
+
 
     /**
      * Bootstrap any application services.
